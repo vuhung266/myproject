cca6f088ba359f688f13d4b12379c026a432ae00
vuhung <hungv@vnpay.vn>
2020-06-03 18:24:14 +0700

    aaa

---
+++ src/demo1/Demo1Page.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+        aaaa
+    </div>
+</template>
+<script>
+import { mapState, mapActions } from 'vuex'
+export default {
+  
+}
+</script>
---
+++ src/login/LoginPage.vue
@@ -0,0 +1,53 @@
+<template>
+    <div>
+        <h2>Đăng nhập</h2>
+        <form @submit.prevent="handleSubmit">
+            <div class="form-group">
+                <label for="username">Tài khoản</label>
+                <input type="text" v-model="username" name="username" class="form-control" :class="{ 'is-invalid': submitted && !username }" />
+                <div v-show="submitted && !username" class="invalid-feedback">Username is required</div>
+            </div>
+            <div class="form-group">
+                <label htmlFor="password">Mật khẩu</label>
+                <input type="password" v-model="password" name="password" class="form-control" :class="{ 'is-invalid': submitted && !password }" />
+                <div v-show="submitted && !password" class="invalid-feedback">Password is required</div>
+            </div>
+            <div class="form-group">
+                <button class="btn btn-primary" :disabled="status.loggingIn">Đăng nhập</button>
+                
+                <router-link to="/register" class="btn btn-link">Đăng ký</router-link>
+            </div>
+        </form>
+    </div>
+</template>
+
+<script>
+import { mapState, mapActions } from 'vuex'
+
+export default {
+    data () {
+        return {
+            username: '',
+            password: '',
+            submitted: false
+        }
+    },
+    computed: {
+        ...mapState('account', ['status'])
+    },
+    created () {
+        // reset login status
+        this.logout();
+    },
+    methods: {
+        ...mapActions('account', ['login', 'logout']),
+        handleSubmit (e) {
+            this.submitted = true;
+            const { username, password } = this;
+            if (username && password) {
+                this.login({ username, password })
+            }
+        }
+    }
+};
+</script>
